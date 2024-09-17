850e0e86dd443dc29f8d936b77cea9def4635eee
nikitaverma18 <161702924+nikitaverma18@users.noreply.github.com>
2024-09-17 16:48:51 +0530

    Add files via upload

---
+++ catTask.js
@@ -0,0 +1,17 @@
+function switchon() {
+    var catimage = document.getElementById("catImage");
+    catimage.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSobr5-nI-r2ZvQNhQyv3cAC-PvbIX_KNctNg&s";
+    
+    
+    var bulbon = document.getElementById("bulbImage");
+    bulbon.src = "https://i.pinimg.com/736x/31/8e/52/318e52abb1d1c010b025ec9087863cb5.jpg";
+}
+
+
+function switchoff() {
+    var catchange = document.getElementById("catImage");
+    catchange.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPMFh4PD8Nn85m-luas0xdFyfqCUXP9e9s9Q&s"
+
+      var bulboff= document.getElementById("bulbImage");
+    bulboff.src = "https://t3.ftcdn.net/jpg/02/52/80/80/360_F_252808017_PSx4my6K1rdtN0kNXfdsUGGLbFD2FOQU.jpg"
+}
---
+++ headChange.js
@@ -0,0 +1,10 @@
+function changeHeading(){
+var head = document.getElementById("Web");
+head.textContent = "4.0 Technologies";
+head.style.color = 'blue';
+head.style.fontSize = '100px';
+head.style.backgroundColor = 'black'
+
+var button = document.getElementById("but")
+button.style.fontSize = '30px'
+}
